22678b01a6c6c799a4075f6a0c8a79908f3711a4
Lesya899 <95936834+Lesya899@users.noreply.github.com>
2024-11-22 16:21:01 +0300

    Update .send_report.sh

--- .send_report.sh
+++ .send_report.sh
@@ -1,5 +1,5 @@
 
-REPORT_PATH="gitleaks-report.json"
+REPORT_PATH="results.sarif"
 if [ -f "$REPORT_PATH" ]; then
     curl -X POST -H "Content-Type: application/json" -d @"$REPORT_PATH" http://localhost:8080/api/report/
 else
