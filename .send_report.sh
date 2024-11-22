5dd69fa8da5eebeb07cdf7a2733db676c85bf0d9
Lesya899 <95936834+Lesya899@users.noreply.github.com>
2024-11-22 15:58:29 +0300

    Create .send_report.sh

---
+++ .send_report.sh
@@ -0,0 +1,7 @@
+
+REPORT_PATH="gitleaks-report.json"
+if [ -f "$REPORT_PATH" ]; then
+    curl -X POST -H "Content-Type: application/json" -d @"$REPORT_PATH" http://localhost:8080/api/report/
+else
+    echo "Отчет Gitleaks не найден."
+fi
