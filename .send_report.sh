fc27337b41638ee639b6ce2f5163d0c151675099
Lesya899 <95936834+Lesya899@users.noreply.github.com>
2024-11-22 16:23:27 +0300

    Delete .send_report.sh

--- .send_report.sh
+++
@@ -1,7 +0,0 @@
-
-REPORT_PATH="results.sarif"
-if [ -f "$REPORT_PATH" ]; then
-    curl -X POST -H "Content-Type: application/json" -d @"$REPORT_PATH" http://localhost:8080/api/report/
-else
-    echo "Отчет Gitleaks не найден."
-fi
